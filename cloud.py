f3040c49fa7175cce15d9876790cc0fa20e95534
Ahsan Raza <aysanraza@gmail.com>
2023-02-27 15:20:29 +0500

    Update_cloud.py

--- cloud.py
+++ cloud.py
@@ -2,19 +2,19 @@
 from nltk.probability import FreqDist
 from wordcloud import WordCloud, ImageColorGenerator
 import matplotlib.pyplot as plt
+import streamlit as st
 
 
 # function defined to build the "word cloud" representation of processed data of pubmed article
 def w_cloud(data):
-    # Use a breakpoint in the code line below to debug your script.
-    mostcommon = FreqDist(data).most_common(200)
-    wordcloud = WordCloud(width=1600, height=800, background_color='white').generate(str(mostcommon))
-    fig = plt.figure(figsize=(30, 10), facecolor='white')
-    plt.imshow(wordcloud, interpolation="bilinear")
-    plt.axis('off')
-    plt.title('Top 100 Most Common Words', fontsize=100)
-    plt.tight_layout(pad=0)
-    plt.show() # Press Ctrl+F8 to toggle the breakpoint.
+        mostcommon = FreqDist(data).most_common(200)
+        wordcloud = WordCloud(width=1600, height=800, background_color='white').generate(str(mostcommon))
+        fig = plt.figure(figsize=(30, 10), facecolor='white')
+        plt.imshow(wordcloud, interpolation="bilinear")
+        plt.axis('off')
+        st.text("")
+        plt.tight_layout(pad=0)
+        st.pyplot(fig)
 
 
 if __name__ == '__main__':
